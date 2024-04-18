56334850c4482433699e07911c3c4cec420ccca7
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-04-18 23:49:15 +0100

    finshed task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,5 @@
+-- a SQL script that ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
 SELECT origin, nb_fans_sum AS total_fans
 FROM (
   SELECT origin, SUM(nb_fans) AS nb_fans_sum
