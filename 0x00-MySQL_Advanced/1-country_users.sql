8759501931ae88c55b6f667431c313fb38639496
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-04-18 23:45:34 +0100

    hot fix: task 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- A script that creates a table with this
 -- attributes id, email, name, country
-CREATE TABLE users (
+CREATE TABLE users IF NOT EXISTS(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
